399170e87fd08377030dcc231f907e9b5f8bc0f5
Murat Odunc <murat.asya@gmail.com>
2015-07-20 12:28:26 +0300

    rename file

---
+++ resources/js/src/modules/socketcon.js
@@ -0,0 +1,109 @@
+import Logger from './logger';
+
+
+/**
+ * Simple Connection Class 
+ * 
+ * This class creates websocket connection to connect to server side in real-time
+ */
+class Connection extends Logger {
+	
+	/**
+	 * Constructor
+	 */
+	constructor(console, driver, hostname, port, path) {
+		
+		super(console);
+
+		this.hostname	= hostname || 'localhost';
+		this.port		= port || 80;
+		this.path		= path || 'ws';
+		this.driver		= driver || WebSocket;
+		
+		// connection to server side
+		this.makeConnection();
+	}
+	
+	/**
+	 * To connect to server via native websocket 
+	 *
+	 * @return void
+	 */
+	makeConnection() {
+
+		var args = this.getConnectionArgs();
+		
+		try {
+			
+			this.connection = new this.driver(args);
+			
+			this.addsDefaultEventOnConnection();
+
+		} catch(Error) {
+
+			super.sendError('Connection is not estabished! ' + Error);
+		}
+
+	}
+	
+	/**
+	 * To adds default event on current connection
+	 */
+	addsDefaultEventOnConnection() {
+
+		this.connection.onerror = function() {
+		   
+			super.sendError('The connection is broken');
+		};
+
+		this.connection.onopen	= function() {
+		   
+			super.sendInfo('The connection is established..'); 
+		};
+	}
+	
+	/**
+	 * To get connection parameters
+	 * @return String
+	 */
+	getConnectionArgs() {
+
+		return 'ws://' + this.hostname + ":" + this.port + "/" + this.path;
+	}
+
+	/**
+	 * To get the readyState property of injected websocket object
+	 *
+	 * @return int
+	 */
+	getReadyState() {
+
+		return this.connection.readyState;
+	}
+
+	/**
+	 * To determine current connection is live
+	 *
+	 * @return bool
+	 */
+	isLive() {
+
+		var state = this.getReadyState();
+		
+		return 1 === state;
+	}
+
+	/**
+	 * To determine current connection is closed
+	 *
+	 * @return bool
+	 */
+	isClosed() {
+
+		return ! this.isLive();
+	}
+
+
+}
+
+module.exports = Connection;
