338c286a86442872b8ccdd0097c2605143bf437d
Murat Odunc <murat.asya@gmail.com>
2015-07-20 12:28:01 +0300

    minor changes

--- resources/js/src/app.js
+++ resources/js/src/app.js
@@ -3,23 +3,16 @@
  */
 
 // importing modules..
-import connection from "./modules/websocket.js";
+import connection from "./modules/socketcon";
 
 
 (function(win, socket, connection) {
 
-		var port		= win.location.port;
-
-		var hostname	= win.location.hostname;
-		
-		var console		= win.console;
-		
-		var live		= new connection(console, socket, hostname, port);
-
-		win.webStat		= live;
-
-
-
+	var port		= win.location.port;
+	var hostname	= win.location.hostname;
+	var console		= win.console;
+	var live		= new connection(console, socket, hostname, port);
+	win.webStat		= live;
 
 })(window, WebSocket,connection); 
 
